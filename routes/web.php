26e2931fe552273ee7f38b1f192ec2d72fbd2e17
Marski <marski-dev@proton.me>
2024-02-03 16:15:01 +0100

    HTML & CSS Templates for Laravel

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/user/{id}', [PostController::class, 'user'])->name('posts.user');
-Route::get('/toggleFollow/{user}', [ProfileController::class, 'toggleFollow'])->name('toggleFollow');
+Route::get('/', [PostController::class, 'index'])
+    ->name('posts.index');
+Route::get('/posts/user/{id}', [PostController::class, 'user'])
+    ->name('posts.user');
+Route::get('/toggleFollow/{user}', [ProfileController::class, 'toggleFollow'])
+    ->middleware(['auth', 'verified'])
+    ->name('toggleFollow');
 
 // Automatyczna obsługa domyślnie utworzonych metod dla Resource full.
-Route::resource('posts', PostController::class);
+Route::resource('posts', PostController::class)
+    ->only(['edit', 'update', 'create', 'store', 'destroy'])
+    ->middleware(['auth', 'verified']);
 
+Route::resource('posts', PostController::class)
+    ->only(['show']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+    Route::post('/profile', [ProfileController::class, 'image'])
+        ->name('profile.image');
 });
 
 require __DIR__ . '/auth.php';
